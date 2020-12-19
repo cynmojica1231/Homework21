83a255f84ce2f42537848c3927b99b82549fd20b
Cynthia Mojica <cmojica1231@gmail.com>
2020-12-19 00:50:04 -0800

    edit seedDB

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
+
 const bookSeed = [
   {
     title: "The Dead Zone",
