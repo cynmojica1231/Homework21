07d18a0849ce37f64f9f6e366ec61b2829e9ba20
Cynthia Mojica <cmojica1231@gmail.com>
2020-12-19 01:16:26 -0800

    modify src search

--- client/src/pages/Search/search.js
+++ client/src/pages/Search/search.js
@@ -2,111 +2,113 @@ import React, { useState, useEffect } from "react";
 import Jumbotron from "../../components/Jumbotron/";
 import API from "../../utils/API";
 import CardBody from "../../components/cardBody/cardBody";
-import Card from "../../components/Card/card"
-import SaveBtn from "../../components/saveBtn/saveBtn"
-import ViewBtn from "../../components/viewBtn/viewBtn"
+import Card from "../../components/Card/card";
+import SaveBtn from "../../components/saveBtn/saveBtn";
+
 import { Col, Row, Container } from "../../components/Grid/";
 import { List, ListItem } from "../../components/List";
 import { Input, FormBtn } from "../../components/Form/form";
 
-import "../../pages/Search/styles.css"
+import "../../pages/Search/styles.css";
 
 function Search() {
-  const [books, setBooks] = useState([])
-  const [searchTerm, setSearchTerm] = useState("")
+  const [books, setBooks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   function loadBooks() {
     API.getBooksByTitle(searchTerm)
-      .then(res => {
-        setBooks(res)
-        }
-      )
-      .catch(err => console.log(err));
-  };
+      .then((res) => {
+        setBooks(res);
+      })
+      .catch((err) => console.log(err));
+  }
 
   function handleInputChange(event) {
     const { value } = event.target;
-    setSearchTerm( value.replace(/\s/g, '') );
-  };
+    setSearchTerm(value.replace(/\s/g, ""));
+  }
 
   function handleSearchSubmit(event) {
     event.preventDefault();
     if (searchTerm) {
       loadBooks();
     }
-  };
-  
+  }
+
   function handleSaveSubmit(bookData) {
     API.saveBook({
-      _id : bookData.id,
+      _id: bookData.id,
       title: bookData.title,
       authors: bookData.authors,
       description: bookData.description,
       image: bookData.image,
-      link: bookData.link
-    })
-  };
+      link: bookData.link,
+    });
+  }
 
-    return (
-      <Container fluid>
-        <Row>
-          <div className="hero">
-            <Jumbotron>
-              <h1>Google Books Search</h1>
-            </Jumbotron>
-          </div>
-          <Col size="md-12">
-            <form className="d-flex">
-              <Input
-                onChange={handleInputChange}
-                name="title"
-                style={{ textAlign: "center", backgroundColor: "rgb(232,240,254)" }}
-                placeholder='Type here a book name'
-              />
-              <FormBtn
-                style={{ textAlign: "center", backgroundColor: "rgb(232,240,254)" }}
-                onClick={handleSearchSubmit}
-              >
-                <i className="fas fa-search"></i>
-              </FormBtn>
-            </form>
-          </Col>
-          <Col size="md-12">
-            <Card>
-              {books.length >0? (
+  return (
+    <Container fluid>
+      <Row>
+        <div className="hero">
+          <Jumbotron>
+            <h1>Google Books Search</h1>
+          </Jumbotron>
+        </div>
+        <Col size="md-12">
+          <form className="d-flex">
+            <Input
+              onChange={handleInputChange}
+              name="title"
+              style={{
+                textAlign: "center",
+                backgroundColor: "rgb(232,240,254)",
+              }}
+              placeholder="Type here a book name"
+            />
+            <FormBtn
+              style={{
+                textAlign: "center",
+                backgroundColor: "rgb(232,240,254)",
+              }}
+              onClick={handleSearchSubmit}
+            >
+              <i className="fas fa-search"></i>
+            </FormBtn>
+          </form>
+        </Col>
+        <Col size="md-12">
+          <Card>
+            {books.length > 0 ? (
               <List>
-                {books.map(book => (
+                {books.map((book) => (
                   <ListItem key={book.id}>
-                      <Card>
+                    <Card>
                       <SaveBtn
-                          handleSaveSubmit={handleSaveSubmit}
-                          bookData={book}
-                        >
-                          <i className="far fa-heart"></i>
-                        </SaveBtn>
-                        <ViewBtn
-                          link={book.link}
-                        />
-                        <CardBody
-                          key={book.id}
-                          title={book.title}
-                          authors={book.authors}
-                          image={book.image}
-                          description={book.description}
-                        />
-                      </Card>
+                        handleSaveSubmit={handleSaveSubmit}
+                        bookData={book}
+                      >
+                        <i className="far fa-heart"></i>
+                      </SaveBtn>
+                      <ViewBtn link={book.link} />
+                      <CardBody
+                        key={book.id}
+                        title={book.title}
+                        authors={book.authors}
+                        image={book.image}
+                        description={book.description}
+                      />
+                    </Card>
                   </ListItem>
                 ))}
               </List>
             ) : (
               <p className="display-message text-center mt-5">No Result</p>
             )}
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 export default Search;
