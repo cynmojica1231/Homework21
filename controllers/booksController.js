e6e2306c8448c2d2f61e07f646bf559cf2307412
Cynthia Mojica <cmojica1231@gmail.com>
2020-12-19 01:13:12 -0800

    edit controllers

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -31,6 +31,5 @@ module.exports = {
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+   
 };
