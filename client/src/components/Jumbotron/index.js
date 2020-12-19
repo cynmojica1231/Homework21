e398bc8916ac71396bc2771066595f5fdb2c2155
Cynthia Mojica <cmojica1231@gmail.com>
2020-12-19 01:06:39 -0800

    fix jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../Jumbotron/style.css";
 
 function Jumbotron({ children }) {
-  return <div style={{ textAlign: "center", height: 300 }}>{children}</div>;
+  return (
+    <div style={{ textAlign: "center", height: 300 }} className="jumbotron">
+      {children}
+    </div>
+  );
 }
 
 export default Jumbotron;
